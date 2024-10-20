7aa4e776f7f0ca32b19314b8957ce4d5fb2a9caf
ZZF-4 <3115767601@qq.com>
2024-10-20 14:41:11 +0800

    first commit

---
+++ build/fusion_of_camera_and_lidar/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/hawkbot/fusion_ws/src/fusion_of_camera_and_lidar
+# Build directory: /home/hawkbot/fusion_ws/build/fusion_of_camera_and_lidar
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
